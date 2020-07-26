02df2122627be0246f722dce77d75d98a875e2b0
nberg002 <nberg002@northeastern.edu>
2020-07-26 14:38:52 -0700

    updating pong3

--- pong3/main.lua
+++ pong3/main.lua
@@ -20,6 +20,9 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+-- speed at which we will move our paddle; multiplied by dt in update
+PADDLE_SPEED = 200
+
 --[[
     Runs when the game first starts up, only once; used to initialize the game.
 ]]
@@ -28,9 +31,13 @@ function love.load()
     -- and graphics; try removing this function to see the difference!
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    --setting font
+    --setting font for title
     smallFont = love.graphics.newFont('font.ttf', 8)
-    love.graphics.setFont(smallFont)
+
+    --setting font for scores
+    scoreFont = love.graphics.newFont('font.ttf', 28)
+
+
 
     -- initialize our virtual resolution, which will be rendered within our
     -- actual window no matter its dimensions; replaces our love.window.setMode call
@@ -40,6 +47,39 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    --initialize player scores
+    p1Score = 0
+    p2Score = 0
+
+    -- paddle positions on the Y axis (they can only move up or down)
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+
+end
+
+--[[
+    Runs every frame, with "dt" passed in, our delta in seconds 
+    since the last frame, which LÖVE2D supplies us.
+]]
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        -- add positive paddle speed to current Y scaled by deltaTime
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        -- add positive paddle speed to current Y scaled by deltaTime
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
 end
 
 --[[
@@ -66,18 +106,26 @@ function love.draw()
     -- to some versions of the original Pong
     --love.graphics.clear(40, 45, 52, 255)
 
+    love.graphics.setFont(smallFont)
     -- draw welcome text toward the top of the screen
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
+    -- draw score on the left and right center of the screen
+    -- need to switch font to draw before actually printing
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(p1Score), VIRTUAL_WIDTH / 2 - 50, 
+        VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(p2Score), VIRTUAL_WIDTH / 2 + 30,
+        VIRTUAL_HEIGHT / 3)
     
     -- paddles are simply rectangles we draw on the screen at certain points,
     -- as is the ball
     
-    --render first paddle (left)
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    --render first paddle (left), now use player's Y variable
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
-    --render second paddle (right)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    --render second paddle (right), now use player's Y variable
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
     --render ball
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
