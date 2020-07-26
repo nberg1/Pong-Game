98f0619009149227583956d509bbb2521f3d419c
nberg002 <nberg002@northeastern.edu>
2020-07-26 13:31:25 -0700

    updating pong1

--- pong1/main.lua
+++ pong1/main.lua
@@ -1,7 +1,7 @@
 --[[
 
-pong-0
-day-0 update
+pong-1
+day-1 update
 
 author: Nicole
 
@@ -11,29 +11,57 @@ your opponent's edge. First to 10 points wins.
 
 ]]
 
+-- https://github.com/Ulydev/push
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 --[[
-	Runs when the game first starts up, only once. 
-	used to initialize the game
+    Runs when the game first starts up, only once; used to initialize the game.
 ]]
 function love.load()
-	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-		fullscreen = false;
-		vsync = true
-	})
+    -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text 
+    -- and graphics; try removing this function to see the difference!
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- initialize our virtual resolution, which will be rendered within our
+    -- actual window no matter its dimensions; replaces our love.window.setMode call
+    -- from the last example
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
 end
 
 --[[
-	Called after update by LOVE2D, used to draw anything
-	to the screen, updated or otherwise.
+    Keyboard handling, called by LÖVE2D each frame; 
+    passes in the key we pressed so we can access.
+]]
+function love.keypressed(key)
+    -- keys can be accessed by string name
+    if key == 'escape' then
+        -- function LÖVE gives us to terminate application
+        love.event.quit()
+    end
+end
+
+--[[
+    Called after update by LÖVE2D, used to draw anything to the screen, 
+    updated or otherwise.
 ]]
 function love.draw()
-	love.graphics.printf(
-		'Hello Pong!',			--text to render
-		0,						--starting X (0 since going to center based on width)
-		WINDOW_HEIGHT / 2 - 6,	--starting Y (halfway down screen)
-		WINDOW_WIDTH,			--nuumber of pixels to center within (entire screen here)
-		'center')				--alignment mode, can be 'center', 'left', or 'right'
+    -- begin rendering at virtual resolution
+    push:apply('start')
+
+    -- condensed onto one line from last example
+    -- note we are now using virtual width and height now for text placement
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end rendering at virtual resolution
+    push:apply('end')
 end
